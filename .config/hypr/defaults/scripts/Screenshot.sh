6e580df52a1e273aece3713c33d9158a686a3dfb
hologramkrypt <iamtheravenhq@gmail.com>
2025-11-22 08:22:51 -0800

    t

---
+++ .config/hypr/defaults/scripts/Screenshot.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Directory to save screenshots
+DIR="$HOME/Pictures/Screenshots"
+
+# Create directory if it doesn't exist
+mkdir -p "$DIR"
+
+# Filename with timestamp
+FILENAME="$DIR/screenshot-$(date +'%Y-%m-%d_%H-%M-%S').png"
+
+# Take screenshot
+hyprshot -z -m region -o $DIR -s
+
+# Optional: notify when saved
+if command -v notify-send >/dev/null; then
+    notify-send "Screenshot saved" "$FILENAME"
+fi
+
--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "pyright", "markdown_oxide" }
+                ensure_installed = { "lua_ls", "markdown_oxide" }
             })
         end
     },
