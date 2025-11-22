8ddd1e5efcf3e81454d4e52a451483938078703c
hologramkrypt <iamtheravenhq@gmail.com>
2025-11-22 09:51:23 -0800

    major

---
+++ dotfiles/.config/rofi/scripts/Powermenu.sh
@@ -0,0 +1,30 @@
+# Rofi Powermenu Script
+@import "~/.config/rofi/colors/tokyonight.rasi"
+
+entries=(
+    " Lock"
+    " Logout"
+    " Reboot"
+    " Shutdown"
+    " Suspend"
+)
+
+selected=$(
+    printf '%s\n' "${entries[@]}" | rofi -dmenu \
+        -theme ~/.config/rofi/themes/juno.rasi \
+        -p "" \
+        -width 20 \
+        -lines 5 \
+        -location 4 \
+        -hide-scrollbar true
+)
+
+case "$selected" in
+    " Lock") exec hyprlock ;;
+    " Logout") hyprctl dispatch exit ;;
+    " Reboot") systemctl reboot ;;
+    " Shutdown") systemctl poweroff ;;
+    " Suspend") systemctl suspend ;;
+    *) exit 0 ;;
+esac
+
